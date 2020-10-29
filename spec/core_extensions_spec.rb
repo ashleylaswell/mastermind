3d7dcb17adaa010534515a673912bc29cd104788
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 19:16:00 -0400

    fixed names of methods in tests

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -14,24 +14,24 @@ describe Array do
 	end
 	context "#any_empty?" do
 		it "returns true if any elements of the Array are empty" do
-			expect(["", "red", ""].all_empty?).to be_truthy
+			expect(["", "red", ""].any_empty?).to be_truthy
 		end
 		it "returns false if there are no empty elements of the Array" do
-			expect(["yellow", "red", "orange"].all_empty?).to be_falsey
+			expect(["yellow", "red", "orange"].any_empty?).to be_falsey
 		end
 		it "returns true for an empty Array" do
-			expect([].all_empty?).to be_truthy
+			expect([].any_empty?).to be_truthy
 		end
 	end
 	context "#none_empty?" do
 		it "returns false if any elements of the Array are full" do
-			expect(["orange", "red", ""].all_empty?).to be_falsey
+			expect(["orange", "red", ""].none_empty?).to be_falsey
 		end
 		it "returns true if there are no empty elements" do
-			expect(["yellow", "red", "orange"].all_empty?).to be_truthy
+			expect(["yellow", "red", "orange"].none_empty?).to be_truthy
 		end
 		it "returns false for an empty Array" do
-			expect([].all_empty?).to be_falsey
+			expect([].none_empty?).to be_falsey
 		end
 	end
 end
