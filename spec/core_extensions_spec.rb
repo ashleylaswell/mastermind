3a63fe3dbd5a994ec0aa25694a9cd11c0730c923
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 19:01:11 -0400

    added tests for all_empty method

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -1,1 +1,15 @@
 require "spec_helper"
+
+describe Array do
+	context "#all_empty?" do
+		it "returns true if all elements of the Array are empty" do
+			expect(["", "", ""].all_empty?).to be_truthy
+		end
+		it "returns false if some of the Array elements are not empty" do
+			expect(["", "red", ""].all_empty?).to be_falsey
+		end
+		it "returns true for an empty Array" do
+			expect([].all_empty?).to be_truthy
+		end
+	end
+end
