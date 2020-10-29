59baa14b3e4d9b54e0679e0b22d3245c78da06d6
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 16:50:03 -0400

    add test for initialize method of board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,11 @@
 require "spec_helper"
+
+module Mastermind
+	describe Board do
+		context "#initialize" do
+			it "initializes the board with a grid" do
+				expect { Board.new(grid: "grid" ) }.to_not raise_error
+			end
+		end
+	end
+end
