a08affd2fcf6f42c1edb86bbf87248d5bf349e79
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 15:36:03 -0400

    add tests for initialize method of player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,15 @@
 require "spec_helper"
+
+module Mastermind
+	describe Player do
+		context "#initialize" do
+			it "raises an exception when initialized with {}" do
+				expect { Player.new({}) }.to raise_error
+			end
+			it "does not raise an error when initialized with a valid input hash" do
+				input = { first_input: "red", second_input: "orange", third_input: "yellow", fourth_input: "green", name: "ashley" }
+				expect { Player.new(input) }.to_not raise_error
+			end
+		end
+	end
+end
