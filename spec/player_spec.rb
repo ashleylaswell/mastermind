1282ab313e3aef6b03bed33bdba466aef24ed506
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 16:29:35 -0400

    add tests for attr_reader

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -11,5 +11,19 @@ module Mastermind
 				expect { Player.new(input) }.to_not raise_error
 			end
 		end
+		context "#second_input" do
+			it "returns the second_input color" do
+				input = { first_input: "red", second_input: "orange", third_input: "yellow", fourth_input: "green", name: "ashley" }
+				player = Player.new(input)
+				expect(player.second_input).to eq "orange"
+			end
+		end
+		context "#name" do
+			it "returns the player's name" do
+				input = { first_input: "red", second_input: "orange", third_input: "yellow", fourth_input: "green", name: "ashley" }
+				player = Player.new(input)
+				expect(player.name).to eq "ashley"
+			end
+		end
 	end
 end
