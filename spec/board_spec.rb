1b4bfefa5f3528ffc20cce072480a16e658b05ac
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 17:08:46 -0400

    add tests for column and row size

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,16 @@ module Mastermind
 			it "initializes the board with a grid" do
 				expect { Board.new(grid: "grid" ) }.to_not raise_error
 			end
+			it "sets the row size to 10 by default" do
+				board = Board.new
+				expect(board.grid.size).to eq(10)
+			end
+			it "sets the column size to 4 by default" do
+				board = Board.new
+				board.grid.each do |row|
+					expect(row.size).to eq(4)
+				end
+			end	
 		end
 	end
 end
