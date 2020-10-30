4dcd88f1a15d737c077afb8f3e2733b62f247105
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 22:43:32 -0400

    add tests to check if initialize method for computer class sets array size to 4

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,12 +3,12 @@ require "spec_helper"
 module Mastermind
 	describe Computer do
 		context "#initialize" do
-			it "raises an exception when initializedd with {}" do
-				expect { Computer.new({}) }.to raise_error
+			it "initializes computer with an array" do
+				expect { Computer.new(computer_array: "computer_array") }.to_not raise_error
 			end
-			it "does not raise an exception with valid input hash" do
-				input = { computer_array: ["red", "orange", "yellow", "blue"], name: "computer" }
-				expect { Computer.new(input) }.to_not raise_error
+			it "sets the array to have 4 elements" do
+				computer = Computer.new
+				expect(computer.computer_array.size).to eq(4)
 			end
 		end
 	end
