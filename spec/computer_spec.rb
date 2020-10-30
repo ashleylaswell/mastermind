cc1b60df6a1a1e327ece6a1da4331477e788dee9
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 21:57:36 -0400

    add tests for initialize method

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -6,6 +6,10 @@ module Mastermind
 			it "raises an exception when initializedd with {}" do
 				expect { Computer.new({}) }.to raise_error
 			end
+			it "does not raise an exception with valid input hash" do
+				input = { computer_array: ["red", "orange", "yellow", "blue"], name: "computer" }
+				expect { Computer.new(input) }.to_not raise_error
+			end
 		end
 	end
 end	
