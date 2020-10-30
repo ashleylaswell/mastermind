e0f09ead3a9fea4689815101ffda8a4fc9414fe3
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 21:03:16 -0400

    setting up computer play class to choose 4 random colors

---
+++ lib/mastermind/computer.rb
@@ -0,0 +1,18 @@
+module Mastermind
+	class Computer
+		attr_reader :computer_array
+		def initialize(input = {})
+			@computer_array = input.fetch(:computer_array, computer_play)
+		end
+
+		def computer_colors
+			color_array = ["red", "orange", "yellow", "green", "blue", "pink"]
+			randomize_colors = color_array.sort { rand() -0.5 }
+			randomize_colors[0..3]
+		end
+		computer_play = computer_colors
+	end
+end
+
+
+
