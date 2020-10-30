3c85569dbbc31002bcfd99b9c102ec1084482218
Ashley Laswell <lswl9898@gmail.com>
2020-10-29 21:48:03 -0400

    fixed test setup

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,9 +1,11 @@
 require "spec_helper"
 
-describe Computer do
-	context "#initialize" do
-		it "raises an exception when initializedd with {}" do
-			expect { Computer.new({}) }.to raise_error
+module Mastermind
+	describe Computer do
+		context "#initialize" do
+			it "raises an exception when initializedd with {}" do
+				expect { Computer.new({}) }.to raise_error
+			end
 		end
 	end
-end
+end	
