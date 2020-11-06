1429733a41511206dd95b6f31e0daca9a09de466
Ashley Laswell <lswl9898@gmail.com>
2020-11-05 19:27:36 -0500

    updating example game

--- example/example_game.rb
+++ example/example_game.rb
@@ -1,9 +1,13 @@
 require_relative "../lib/mastermind.rb"
 
 puts "Welcome to Mastermind"
+puts "Mastermind is a game with a codemaker and a codebreaker. The computer is the codemaker and will pick 4 colors in a specific order. You will guess the colors in order and after your 4 guesses, the computer will tell you how many colors you got correct and how many are in the correct position."
+puts "Enter 4 colors and press enter after each color"
+puts "Your choices of colors are red, orange, yellow, green, blue, and pink."
 
-ashley = Mastermind::Player.new
+ashley = Mastermind::Player.new({name: "ashley"})
 computer = Mastermind::Computer.new
 
 players = [ashley, computer]
-Mastermind::Game.new(players).play
+game = Mastermind::Game.new(players)
+game.play(game.turn)
